ae4e00a12249fe884bc92c128f6afc70e5ef0eea
Victor Hernandez-Urbina <hernandezurbina@vico.local>
2018-09-08 15:55:35 +0100

    Bullet shooting added

--- ARShooter/ViewController.swift
+++ ARShooter/ViewController.swift
@@ -15,12 +15,45 @@ class ViewController: UIViewController {
     
     let configuration = ARWorldTrackingConfiguration()
     
+    var power: Float = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.autoenablesDefaultLighting = true
         self.sceneView.session.run(configuration)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let pointOfView = sceneView.pointOfView else {return}
+        
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let position = orientation + location
+        
+        let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        
+        bullet.position = position
+        
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet, options: nil))
+        body.isAffectedByGravity = false
+        bullet.physicsBody = body
+        
+        bullet.physicsBody?.applyForce(SCNVector3(orientation.x * power, orientation.y * power, orientation.z * power), asImpulse: true)
+        
+        self.sceneView.scene.rootNode.addChildNode(bullet)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,5 +75,9 @@ class ViewController: UIViewController {
         self.sceneView.scene.rootNode.addChildNode(eggNode!)
     }
     
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
