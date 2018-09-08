ac812440595310e86e2963b0a68454bc4a493d10
Victor Hernandez-Urbina <hernandezurbina@vico.local>
2018-09-08 15:28:27 +0100

    Added basic functionality. Displays eggs in scene.

--- ARShooter/ViewController.swift
+++ ARShooter/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.autoenablesDefaultLighting = true
+        self.sceneView.session.run(configuration)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addTargets(_ sender: Any) {
+        self.addEgg(x: 5, y: 0, z: -40)
+        self.addEgg(x: 0, y: 0, z: -40)
+        self.addEgg(x: -5, y: 0, z: -40)
+    }
+    
+    func addEgg(x: Float, y: Float, z: Float){
+        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
+        let eggNode = eggScene?.rootNode.childNode(withName: "egg", recursively: false)
+        eggNode?.position = SCNVector3(x, y, z)
+        
+        self.sceneView.scene.rootNode.addChildNode(eggNode!)
+    }
+    
 }
 
